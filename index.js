147f8daf7428d0bdbda42b5b94b0eff3f0f75917
katrin frisilia kembuan <64717121+Katrine23@users.noreply.github.com>
2020-11-11 15:38:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -57,6 +57,19 @@ function hitung() {
 	tinggi /= 100;
 	berat = parseFloat(document.getId("in_put").value);
 	bmi = berat / (tinggi * tinggi);
-
+	
+	
+	if (bmi > 27) {
+		keterangan = "Gemuk, Kelebihan berat badan tingkat berat";
+	}else if ((bmi >= 25.1) & (bmi <= 27)){
+		keterangan = "Gemuk, Kelebihan berat badan tingkat ringan";
+	}else if ((bmi >= 18.5) & (bmi <= 25)){
+		keterangan = "Normal";
+	} else if ((bmi >= 17) & (bmi <= 18.4)){
+		keterangan = "Kurus, Kekurangan berat badan tingkat ringan";
+	}else {
+		keterangan = "Kurus, Kekurangan berat badan tingkat berat";
+	}
+	document.getId('keterangan').innerHTML = keterangan;
 	document.getId('bmi').innerHTML = "Hasil perhitungan BMI : " + bmi.toFixed(1);
 }
